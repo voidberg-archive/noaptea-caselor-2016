0affea3a9f617c026053dd102e017b9c517af868
Alexandru Badiu <andu@ctrlz.ro>
2016-10-07 18:40:57 +0300

    Content update.

--- _harp/public/assets/js/app.js
+++ _harp/public/assets/js/app.js
@@ -1,4 +1,17 @@
 $(document).ready(function ready() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
   $('.house-carousel').each(function iterator(i, carouselContainer) {
     var numImages = $('> div', carouselContainer).length;
 
